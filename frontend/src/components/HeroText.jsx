266ca1823f45f7205f17dc042079f7e97fae4813
Balia077 <balaramd894@gmail.com>
2026-04-11 12:21:16 +0530

    feat: add nutrition image to HeroText component and adjust layout

--- frontend/src/components/HeroText.jsx
+++ frontend/src/components/HeroText.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const HeroText = () => {
   return (
-    <div className='w-full h-[85vh] p-10'>
+    <div className='w-full h-[85vh] p-10 flex'>
       <h1 className='heroText py-15 font-[thunder-font] text-black tracking-wider text-8xl leading-15 uppercase'>Your Personal <br /> <span className='sensei font-[font-1] leading-20 tracking-wider z-10 text-[23vh] text-[#F30819] capitalize'>Sensei</span> <br /> </h1>
-      <span className='heroText z-1 font-[thunder-font] text-black tracking-wider text-8xl absolute top-84.5 uppercase'>AI Nutrition</span>
+      <span className='heroText z-1 font-[thunder-font] text-black tracking-wider text-8xl absolute top-86.5 uppercase'>AI Nutrition</span>
+      <div className='h-17.5 w-30 top-87 absolute left-115'>
+        <img className='h-full w-full object-cover rounded-xl overflow-hidden' src="/images/nutrition.webp" alt="" />
+      </div>
     </div>
   )
 }
