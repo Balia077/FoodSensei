e8774344ae3033d3d90b13eb2a90cf3cdc0c5c62
Balia077 <balaramd894@gmail.com>
2026-04-11 11:37:52 +0530

    feat: add HeroText component and integrate into Home page

---
+++ frontend/src/components/HeroText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='w-full h-[85vh] p-10'>
+      <h1 className='heroText py-20 font-[thunder-font] text-black tracking-wider text-9xl leading-25 uppercase'>Your Personal <br /> AI Nutrition <br /> <span className='sensei font-[font-1] leading-10 tracking-wider text-[25vh] text-[#F30819] capitalize'>Sensei</span></h1>
+    </div>
+  )
+}
+
+export default HeroText
--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroText from '../components/HeroText'
 
 const Home = () => {
   return (
     <div className='h-screen w-full bg-[#F8F2F0]'>
       <Navbar />
+      <HeroText />
     </div>
   )
 }
