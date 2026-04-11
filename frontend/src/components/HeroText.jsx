f58084f5fbe1f3a63ed7b69ea3ce3c1413067898
Balia077 <balaramd894@gmail.com>
2026-04-11 12:04:23 +0530

    fix: update HeroText layout and styling for improved readability

--- frontend/src/components/HeroText.jsx
+++ frontend/src/components/HeroText.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const HeroText = () => {
   return (
     <div className='w-full h-[85vh] p-10'>
-      <h1 className='heroText py-20 font-[thunder-font] text-black tracking-wider text-9xl leading-25 uppercase'>Your Personal <br /> AI Nutrition <br /> <span className='sensei font-[font-1] leading-10 tracking-wider text-[25vh] text-[#F30819] capitalize'>Sensei</span></h1>
+      <h1 className='heroText py-15 font-[thunder-font] text-black tracking-wider text-8xl leading-15 uppercase'>Your Personal <br /> <span className='sensei font-[font-1] leading-20 tracking-wider z-10 text-[23vh] text-[#F30819] capitalize'>Sensei</span> <br /> </h1>
+      <span className='heroText z-1 font-[thunder-font] text-black tracking-wider text-8xl absolute top-84.5 uppercase'>AI Nutrition</span>
     </div>
   )
 }
